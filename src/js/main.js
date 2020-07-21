f53fe807ad48d0a5c232aa4ed61078cf01cbb573
isradin <isradin.nurbek@gmail.com>
2020-07-21 14:19:37 +0300

    create: added Keyboard Control in js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@ var mySwiper = new Swiper('.swiperContainer', {
     // Optional parameters
     loop: true,
 
+    // Keyboard Control Parameters
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
     // Navigation arrows
     navigation: {
         nextEl: '.sliderBtnNext',
