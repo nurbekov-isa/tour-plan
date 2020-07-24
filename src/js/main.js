1f358702c38776919663a6a00770b05ee55d815d
isradin <isradin.nurbek@gmail.com>
2020-07-24 16:48:27 +0300

    create: added reviews section and fixed hotel slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var mySwiper = new Swiper('.swiperContainer', {
+/* Hotel Slider */
+var hotelSlider = new Swiper('.hotelSlider', {
     // Optional parameters
     loop: true,
 
@@ -12,7 +13,26 @@ var mySwiper = new Swiper('.swiperContainer', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.sliderBtnNext',
-        prevEl: '.sliderBtnPrev',
+        nextEl: '.hotelSliderBtnNext',
+        prevEl: '.hotelSliderBtnPrev',
+    },
+});
+
+
+/* Hotel Slider */
+var reviewsSlider = new Swiper('.reviewsSlider', {
+    // Optional parameters
+    loop: true,
+
+    // Keyboard Control Parameters
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.reviewsSliderBtnNext',
+        prevEl: '.reviewsSliderBtnPrev',
     },
 });
