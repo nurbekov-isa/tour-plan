5a4e8a83691e24481c36a74563b68b47524c48e7
isradin <isradin.nurbek@gmail.com>
2020-08-06 05:34:01 +0300

    create: added favicon

--- src/send.php
+++ src/send.php
@@ -15,6 +15,7 @@ $title = "Новое обращение Best Tour Plan";
 $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
+<b>Почта:</b> $email<br><br>
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
 ";
