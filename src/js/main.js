e35cc1dd4c93cc8fcce8b2e262bd849ac7d1b232
isradin <isradin.nurbek@gmail.com>
2020-08-07 18:52:39 +0300

    create: fixed php

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,30 @@
 $(document).ready(function() {
-
-    /* Hotel Slider */
-    var hotelSlider = new Swiper('.js-hotelSlider', {
-        // Optional parameters
-        loop: true,
-
-        // Keyboard Control Parameters
-        keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.js-hotelSliderBtnNext',
-            prevEl: '.js-hotelSliderBtnPrev',
-        },
-    });
-
-
-    /* Review Slider */
-    var reviewsSlider = new Swiper('.js-reviewsSlider', {
-        // Optional parameters
-        loop: true,
-        autoHeight: true,
-
-        // Keyboard Control Parameters
-        keyboard: {
-            enabled: true,
-            onlyInViewport: false,
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.js-reviewsSliderBtnNext',
-            prevEl: '.js-reviewsSliderBtnPrev',
-        },
-    });
-
-
-    /* Nav menu button */
-    var menuButton = $('.js-menuBtn');
-    menuButton.on('click', function() {
-        $('.js-navMenu').toggleClass('nav_mobile_visible');
-    });
-
-
-    /* Modal */
-    var modalBtn = $('[data-toggle=modal]');
-    var closeModalBtn = $('.js-closeModal');
-
-    modalBtn.on('click', openModal);
-    closeModalBtn.on('click', closeModal);
-
-    function openModal() {
-        var modalOverlay = $('.js-modalOverlay');
-        var modalDialog = $('.js-modalDialog');
-
-        modalOverlay.addClass('modal__overlay_visible');
-        modalDialog.addClass('modal__dialog_visible');
+    function e() {
+        var e = $(".js-modalOverlay"),
+            a = $(".js-modalDialog");
+        e.addClass("modal__overlay_visible"), a.addClass("modal__dialog_visible")
     }
 
-    function closeModal(event) {
-        event.preventDefault();
-
-        var modalOverlay = $('.js-modalOverlay');
-        var modalDialog = $('.js-modalDialog');
-
-        modalOverlay.removeClass('modal__overlay_visible');
-        modalDialog.removeClass('modal__dialog_visible');
+    function a(e) {
+        e.preventDefault();
+        var a = $(".js-modalOverlay"),
+            o = $(".js-modalDialog");
+        a.removeClass("modal__overlay_visible"), o.removeClass("modal__dialog_visible")
     }
-
-    $(document).keyup(function(e) {
-        if (e.keyCode == 27) {
-
-            var modalOverlay = $('.js-modalOverlay');
-            var modalDialog = $('.js-modalDialog');
-
-            modalOverlay.removeClass('modal__overlay_visible');
-            modalDialog.removeClass('modal__dialog_visible');
+    var o = (new Swiper(".js-hotelSlider", { loop: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-hotelSliderBtnNext", prevEl: ".js-hotelSliderBtnPrev" } }), new Swiper(".js-reviewsSlider", { loop: !0, autoHeight: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-reviewsSliderBtnNext", prevEl: ".js-reviewsSliderBtnPrev" } }), $(".js-menuBtn"));
+    o.on("click", function() { $(".js-navMenu").toggleClass("nav_mobile_visible") });
+    var l = $("[data-toggle=modal]"),
+        i = $(".js-closeModal");
+    l.on("click", e), i.on("click", a), $(document).keyup(function(e) {
+        if (27 == e.keyCode) {
+            var a = $(".js-modalOverlay"),
+                o = $(".js-modalDialog");
+            a.removeClass("modal__overlay_visible"), o.removeClass("modal__dialog_visible")
         }
-    });
+    }), $(".js-form").each(function() { $(this).validate({ errorClass: "invalid", messages: { name: { required: "Please specify your name" }, email: { required: "We need your email address to contact you", email: "Your email address must be in the format of name@domain.com" }, phone: { required: "Your phone is required" } } }) }), $(".js-phone").each(function() { $(this).mask("+7 (000) 000-00-00") }), AOS.init()
+});
 
-    /* Form validation */
-    $('.js-form').each(function() {
-        $(this).validate({
-            errorClass: 'invalid',
-            messages: {
-                name: {
-                    required: 'Please specify your name',
-                },
-                email: {
-                    required: 'We need your email address to contact you',
-                    email: 'Your email address must be in the format of name@domain.com'
-                },
-                phone: {
-                    required: 'Your phone is required'
-                },
-            },
-        });
-    });
-
-    /* jQuery Mask Input */
-    $('.js-phone').each(function() {
-        $(this).mask('+7 (000) 000-00-00');
-    });
-
-    AOS.init();
-
+const map = document.querySelector('.map-wrap');
+map.addEventListener('mouseover', function() {
+    map.insertAdjacentHTML('afterBegin', '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28188.37990398469!2d79.81368106542256!3d7.569713125818822!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sen!2sde!4v1596808010246!5m2!1sen!2sde" allowfullscreen="" aria-hidden="false" tabindex="0" class="map"></iframe>');
 });
--- src/send.php
+++ src/send.php
@@ -4,35 +4,22 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 
-// Формирование самого письма
-$title = "Новое обращение Best Tour Plan";
-$body = "
-<h2>Новое обращение</h2>
-<b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
-";
 
 
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    // $mail->isSMTP();   
+try {   
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
-    // $mail->SMTPDebug = 2;
+    // проверка на ошибку
+    $mail->SMTPDebug = 2; 
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'isradin.110593@gmail.com'; // Логин на почте
+    $mail->Password   = 'Frontend2019'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('isradin.110593@gmail.com', 'Isradin Nurbekov'); // Адрес самой почты и имя отправителя
@@ -40,10 +27,44 @@ try {
     // Получатель письма
     $mail->addAddress('isradin.nurbek@yandex.ru');  
 
-    // Отправка сообщения
+    if (isset($_POST['submit_1'])){
+    $email = $_POST['email'];
+    
+    // отправка почты
+    $title_1 = "Новое обращение Best Tour Plan";
+    $body_1 = "
+    <h2>New subscriber</h2>
+    <b>Message:</b><br>$email
+    ";
+
     $mail->isHTML(true);
-    $mail->Subject = $title;
-    $mail->Body = $body;    
+    $mail->Subject = $title_1;
+    $mail->Body = $body_1;
+
+    header('Location: gratitude.html');
+    }
+
+    if(isset($_POST['submit_2'])) {
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Формирование самого письма
+    $title_2 = "Новое обращение Best Tour Plan";
+    $body_2 = "
+    <h2>New appeal</h2>
+    <b>Name:</br> $name<br>
+    <b>Phone:</br> $phone<br><br>
+    <b>Message:</b><br>$message
+    ";
+    
+    $mail->isHTML(true);
+    $mail->Subject = $title_2;
+    $mail->Body = $body_2;
+    
+    header('Location: gratitude.html');
+    }
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
@@ -55,4 +76,4 @@ else {$result = "error";}
 }
 
 // Отображение результата
-header('Location: gratitude.html');
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
