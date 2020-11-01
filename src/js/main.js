f76d0666b9ef5d58fbc124fc3a5c9cc974fb2a76
Isradin <isradin.nurbek@gmail.com>
2020-11-01 20:28:56 +0300

    Updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,15 @@
 $(document).ready(function() {
-    function e() {
-        var e = $(".js-modalOverlay"),
-            a = $(".js-modalDialog");
-        e.addClass("modal__overlay_visible"), a.addClass("modal__dialog_visible")
-    }
-
-    function a(e) {
-        e.preventDefault();
-        var a = $(".js-modalOverlay"),
+    function e() { var e = $(".js-modalOverlay"),
             o = $(".js-modalDialog");
-        a.removeClass("modal__overlay_visible"), o.removeClass("modal__dialog_visible")
-    }
-    var o = (new Swiper(".js-hotelSlider", { loop: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-hotelSliderBtnNext", prevEl: ".js-hotelSliderBtnPrev" } }), new Swiper(".js-reviewsSlider", { loop: !0, autoHeight: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-reviewsSliderBtnNext", prevEl: ".js-reviewsSliderBtnPrev" } }), $(".js-menuBtn"));
-    o.on("click", function() { $(".js-navMenu").toggleClass("nav_mobile_visible") });
-    var l = $("[data-toggle=modal]"),
-        i = $(".js-closeModal");
-    l.on("click", e), i.on("click", a), $(document).keyup(function(e) {
-        if (27 == e.keyCode) {
-            var a = $(".js-modalOverlay"),
-                o = $(".js-modalDialog");
-            a.removeClass("modal__overlay_visible"), o.removeClass("modal__dialog_visible")
-        }
-    }), $(".js-form").each(function() { $(this).validate({ errorClass: "invalid", messages: { name: { required: "Please specify your name" }, email: { required: "We need your email address to contact you", email: "Your email address must be in the format of name@domain.com" }, phone: { required: "Your phone is required" } } }) }), $(".js-phone").each(function() { $(this).mask("+7 (000) 000-00-00") }), AOS.init()
-});
+        e.addClass("modal__overlay_visible"), o.addClass("modal__dialog_visible") }
 
-const map = document.querySelector('.map-wrap');
-map.addEventListener('mouseover', function() {
-    map.insertAdjacentHTML('afterBegin', '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28188.37990398469!2d79.81368106542256!3d7.569713125818822!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sen!2sde!4v1596808010246!5m2!1sen!2sde" allowfullscreen="" aria-hidden="false" tabindex="0" class="map"></iframe>');
-});
+    function o(e) { e.preventDefault(); var o = $(".js-modalOverlay"),
+            a = $(".js-modalDialog");
+        o.removeClass("modal__overlay_visible"), a.removeClass("modal__dialog_visible") } var a = (new Swiper(".js-hotelSlider", { loop: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-hotelSliderBtnNext", prevEl: ".js-hotelSliderBtnPrev" } }), new Swiper(".js-reviewsSlider", { loop: !0, autoHeight: !0, keyboard: { enabled: !0, onlyInViewport: !1 }, navigation: { nextEl: ".js-reviewsSliderBtnNext", prevEl: ".js-reviewsSliderBtnPrev" } }), $(".js-menuBtn"));
+    a.on("click", function() { $(".js-navMenu").toggleClass("nav_mobile_visible") }); var i = $("[data-toggle=modal]"),
+        n = $(".js-closeModal");
+    i.on("click", e), n.on("click", o), $(document).keyup(function(e) { if (27 == e.keyCode) { var o = $(".js-modalOverlay"),
+                a = $(".js-modalDialog");
+            o.removeClass("modal__overlay_visible"), a.removeClass("modal__dialog_visible") } }), $(".js-form").each(function() { $(this).validate({ errorClass: "invalid", messages: { name: { required: "Please specify your name" }, email: { required: "We need your email address to contact you", email: "Your email address must be in the format of name@domain.com" }, phone: { required: "Your phone is required" } } }) }), $(".js-phone").each(function() { $(this).mask("+7 (000) 000-00-00") }), AOS.init() });
+const e = document.querySelector(".map-wrap");
+e.addEventListener("mouseover", function() { e.insertAdjacentHTML("afterBegin", '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d28188.37990398469!2d79.81368106542256!3d7.569713125818822!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fd7668bc2d85eb9!2sGrand%20Hilton%20Hotel!5e0!3m2!1sen!2sde!4v1596808010246!5m2!1sen!2sde" allowfullscreen="" aria-hidden="false" tabindex="0" class="map"></iframe>') });
--- src/send.php
+++ src/send.php
@@ -1,79 +1,10 @@
-<?php
-// Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
-
-
-
-
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    // проверка на ошибку
-    $mail->SMTPDebug = 2; 
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = 'isradin.110593@gmail.com'; // Логин на почте
-    $mail->Password   = 'Frontend2019'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('isradin.110593@gmail.com', 'Isradin Nurbekov'); // Адрес самой почты и имя отправителя
-
-    // Получатель письма
-    $mail->addAddress('isradin.nurbek@yandex.ru');  
-
-    if (isset($_POST['submit_1'])){
-    $email = $_POST['email'];
-    
-    // отправка почты
-    $title_1 = "Новое обращение Best Tour Plan";
-    $body_1 = "
+<?php require 'phpmailer/PHPMailer.php';require 'phpmailer/SMTP.php';require 'phpmailer/Exception.php';$mail=new PHPMailer\PHPMailer\PHPMailer();try{$mail->CharSet="UTF-8";$mail->SMTPAuth=true;$mail->Debugoutput=function($str,$level){$GLOBALS['status'][]=$str;};$mail->Host='smtp.gmail.com';$mail->Username='isradin.110593@gmail.com';$mail->Password='Frontend2019';$mail->SMTPSecure='ssl';$mail->Port=465;$mail->setFrom('isradin.110593@gmail.com','Isradin Nurbekov');$mail->addAddress('isradin.nurbek@yandex.ru');if(isset($_POST['submit_1'])){$email=$_POST['email'];$title_1="Новое обращение Best Tour Plan";$body_1="
     <h2>New subscriber</h2>
     <b>Message:</b><br>$email
-    ";
-
-    $mail->isHTML(true);
-    $mail->Subject = $title_1;
-    $mail->Body = $body_1;
-
-    header('Location: gratitude.html');
-    }
-
-    if(isset($_POST['submit_2'])) {
-    // Переменные, которые отправляет пользователь
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-
-    // Формирование самого письма
-    $title_2 = "Новое обращение Best Tour Plan";
-    $body_2 = "
+    ";$mail->isHTML(true);$mail->Subject=$title_1;$mail->Body=$body_1;header('Location: gratitude.html');}if(isset($_POST['submit_2'])){$name=$_POST['name'];$phone=$_POST['phone'];$email=$_POST['email'];$message=$_POST['message'];$title_2="Новое обращение Best Tour Plan";$body_2="
     <h2>New appeal</h2>
     <b>Name:</br> $name<br>
     <b>Phone:</br> $phone<br><br>
+    <b>Email:</br> $email<br><br>
     <b>Message:</b><br>$message
-    ";
-    
-    $mail->isHTML(true);
-    $mail->Subject = $title_2;
-    $mail->Body = $body_2;
-    
-    header('Location: gratitude.html');
-    }
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";} 
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
-}
-
-// Отображение результата
-// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+    ";$mail->isHTML(true);$mail->Subject=$title_2;$mail->Body=$body_2;header('Location: gratitude.html');}if($mail->send()){$result="success";}else{$result="error";}}catch(Exception $e){$result="error";$status="Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";}
